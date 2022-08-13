1085c48da7903eac1f0e51f542b80c9989caf9f6
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-08-13 14:44:47 -0400

    Delete GUI directory

--- GUI/ui_dialogo.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QDialog
-from nucleo.pantallas.GUI_Dialogo import Ui_Dialog
-
-class GUIDialogo(QMainWindow):
-    def __init__(self, parent = None):
-        super(GUIDialogo, self).__init__(parent)
-        self.uiDialogo = Ui_Dialog()
-        self.uiDialogo.setupUi(self)
