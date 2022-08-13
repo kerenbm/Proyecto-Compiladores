bed40850a3f89ce1c3e8b40d4442f912156f2d1c
kerenbm <82061667+kerenbm@users.noreply.github.com>
2022-08-13 14:40:54 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from nucleo.GUI.ui_menu import GUIMenu
+from nucleo.pantallas import recursosMenu
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    main = GUIMenu()
+    main.show()
+    sys.exit(app.exec_())
